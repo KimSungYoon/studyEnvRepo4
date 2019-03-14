4d865fb175444585e2bcfa000ad4f642d9019c5f
82305 <82305@LCNC01V0496.corp.lgcns.com>
2019-03-14 17:56:42 +0900

    hello file pushed

--- src/main/java/hello/StudyEnv4Application.java
+++ src/main/java/hello/StudyEnv4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyEnv4Application {
 
 	public static void main(String[] args) {
+		System.out.println("MyHello");
 		SpringApplication.run(StudyEnv4Application.class, args);
 	}
 
