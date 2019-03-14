616feb774953008065fd48423d1a1c9a3b82e644
82305 <82305@LCNC01V0496.corp.lgcns.com>
2019-03-14 17:42:37 +0900

    initial push

---
+++ src/main/java/hello/StudyEnv4Application.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEnv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyEnv4Application.class, args);
+	}
+
+}
