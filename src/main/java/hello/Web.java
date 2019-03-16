041b7e4facdb8c30159e5cc8e96b52db044bf4d3
82305 <82305@LCNC01V0496.corp.lgcns.com>
2019-03-17 03:23:52 +0900

    Web.java Added

---
+++ src/main/java/hello/Web.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Web {
+
+	@RequestMapping
+	public String home() {
+		return "hello studyEnv4!!";
+	}
+	
+}
